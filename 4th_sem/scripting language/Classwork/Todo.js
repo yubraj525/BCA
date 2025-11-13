0a983602b448be4dc59a0b42fdcbab8aca1e4877
yubraj525 <yubrajkhatri525@gmail.com>
2025-11-13 21:33:06 +0545

    completed todo list :

--- 4th_sem/scripting language/Classwork/Todo.js
+++ 4th_sem/scripting language/Classwork/Todo.js
@@ -1,37 +1,60 @@
-const task=[];
-const addTaskBtn=document.getElementById('addTaskBtn');
-const taskInput=document.getElementById('taskInput');
-const taskList=document.getElementById('content');
-addTaskBtn.addEventListener('click',()=>{
-    const taskText=taskInput.value
-    if(taskText!==''){
-        const newTask={
-            id:task.length+1,
-            text:taskText,
-            completed:false
+const task = [];
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('content');
+const clearall = document.getElementById('clearall');
+
+addTaskBtn.addEventListener('click', () => {
+    const taskText = taskInput.value
+    if (taskText !== '') {
+        const newTask = {
+            id: task.length + 1,
+            text: taskText,
+            completed: false
         };
         task.push(newTask);
         ShowTask();
-        taskInput.value='';
+        taskInput.value = '';
     }
 });
 
-function ShowTask(){
-    task.forEach((taskItem)=>{
-        taskList.innerHTML+=`
+clearall.addEventListener('click', () => {
+    task.forEach((taskItem) => {
+        if (!taskItem.completed) {
+            taskItem.completed = true;
+        }
+        else {
+            taskItem.completed = false;
+        }
+        ShowTask();
+    });
+});
+
+    function ShowTask() {
+        taskList.innerHTML = ``
+        task.forEach((taskItem) => {
+            taskList.innerHTML += `
         <tr class="task-item">
            <td> ${taskItem.id}</td>
-            <td class="${taskItem.completed ?'notcompleted':''}">${taskItem.text}</>
-            <td ><button onclick='toggledone(${taskItem.text})'>Done</button></td>
-        `;  
+            <td  style="color: ${taskItem.completed ? 'green' : 'red'};">${taskItem.text}</>
+            <td >
+            <button onclick='togglestate(${taskItem.id})'>
+            ${taskItem.completed ? 'Undo' : 'Done'}
+            </button>
+            </td>
+        `;
+
+        }
+        );
+    }
+    function togglestate(id) {
+        console.log(id);
+        const taskItem = task.find((item) => item.id === id);
+        taskItem.completed = !taskItem.completed;
+        console.log(taskItem);
+
+        ShowTask();
+      
 
     }
-    );
-}
-function toggledone(text){
-    console.log(text);
-    const taskItem=task.find((item)=>item.text===text);
-  console.log(taskItem);
-    alert("task completed");
 
-}
